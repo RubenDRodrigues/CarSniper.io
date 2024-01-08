dfb44331fc3edca2ff9760bcde3a104fde4de1f2
JustPutin <81159847+JustPutin@users.noreply.github.com>
2024-01-08 22:40:55 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,14 +1,34 @@
-// Replace this config with your Firebase project config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
+  apiKey: "AIzaSyCmCcnRPWb5J8nZiPR_zWViUA3MDNBqPRE",
+  authDomain: "carsniper-fe526.firebaseapp.com",
+  databaseURL: "https://carsniper-fe526-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "carsniper-fe526",
+  storageBucket: "carsniper-fe526.appspot.com",
+  messagingSenderId: "873602962190",
+  appId: "1:873602962190:web:05a721b986cf24cccb3d5f",
+  measurementId: "G-9ZP4616YZS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const database = firebase.database();
+const ref = database.ref();
+
+ref.once("value")
+.then(snapshot => {
+  const data = snapshot.val();
+  console.log("Data from Firebase:", data);
+})
+.catch(error => {
+  console.error("Error fetching data:", error);
+});
