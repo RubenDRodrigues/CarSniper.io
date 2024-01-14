0952d186e84b873fb259d63491928b22e45e16ef
JustPutin <81159847+JustPutin@users.noreply.github.com>
2024-01-14 17:13:08 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase,ref, get,onValue ,limit, set,  query, orderByKey ,endAt } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
-
+import { collection, query, orderBy, startAfter, limit, getDocs } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
