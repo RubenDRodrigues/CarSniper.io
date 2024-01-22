af31bbe9585689617b8fed337e765be93d620beb
JustPutin <81159847+JustPutin@users.noreply.github.com>
2024-01-22 18:29:24 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { get,getDatabase, ref, query,limitToLast,limitToFirst,orderByKey } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+import { get,getDatabase,  query,limitToLast,limitToFirst,orderByKey } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +22,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase();
 
-var ref = db.ref("anuncios");
-var getPromise = ref.once("name", function(snapshot) {
+var lolref = db.ref("anuncios");
+var getPromise = lolref.once("name", function(snapshot) {
   console.log(snapshot.val());
 });
