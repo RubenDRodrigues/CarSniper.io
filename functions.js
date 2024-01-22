6079d5af777160c5de9c491f18511ea2e62d730c
JustPutin <81159847+JustPutin@users.noreply.github.com>
2024-01-22 18:37:33 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { get,getDatabase,  query,limitToLast,limitToFirst,orderByKey } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+import { get,getDatabase, ref, query,limitToLast,limitToFirst,orderByKey } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +22,12 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase();
 
-var lolref = db.ref("anuncios");
-var getPromise = lolref.once("name", function(snapshot) {
-  console.log(snapshot.val());
-});
+var carRef = db.ref("anuncios");
+
+carRef.orderByKey().once('value').then(function(snapshot){
+  snapshot.forEach(childSnapshot => { 
+
+  let snapdb = childSnapshot.val()
+  console.log(snapdb);
+
+  })})
